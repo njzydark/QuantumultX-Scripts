9d7b6482049de49de22eb7f6b62bec51c4d21499
njzy <njzydark@gmail.com>
2021-02-11 12:13:12 +0800

    feat(task/github): support cache username

--- src/task/github/index.ts
+++ src/task/github/index.ts
@@ -1,10 +1,11 @@
-const UserName = 'njzydark';
+import { cacheInit } from '@utils/cache';
 
-const BaseUrl = 'https://api.github.com';
-// https://docs.github.com/en/rest/reference/users#get-a-user
-const GetUserApi = `${BaseUrl}/users/njzydark`;
-// https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
-const ListRepoApi = `${BaseUrl}/users/${UserName}/repos?type=owner&per_page=100`;
+type UserInfo = {
+  name: string;
+  followers: number;
+  following: number;
+  [prop: string]: any;
+};
 
 type RepoInfo = {
   id: string;
@@ -16,13 +17,6 @@ type RepoInfo = {
   watchers_count: number;
   forks_count: number;
   open_issues: number;
-  [prop: string]: any;
-};
-
-type UserInfo = {
-  name: string;
-  followers: number;
-  following: number;
   [prop: string]: any;
 };
 
@@ -38,61 +32,87 @@ type ListRepoResponse = QuanXResponse & {
     /** The time at which the current rate limit window resets in UTC epoch seconds. */
     'X-RateLimit-Reset': number;
     /** The Link header includes pagination information */
-    Link: string;
+    Link?: string;
   };
 };
 
-const fetchUserInfo = async () => {
-  const { body } = await $task.fetch({ url: GetUserApi });
-  const parseBody = JSON.parse(body) as UserInfo;
-  return parseBody;
+type GithubBootstrapOptions = {
+  key: string;
+  username: string;
 };
 
-const fetchAllRepo = async (
-  allData = {
-    total_star_count: 0,
-    total_issuses_count: 0,
-    detail: [] as string[]
-  },
-  page = 1
-) => {
-  const { body, headers } = await $task.fetch<ListRepoResponse>({ url: `${ListRepoApi}&page=${page}` });
-  const parseBody = JSON.parse(body) as RepoInfo[];
-  if (body?.length > 0) {
-    let data = parseBody
-      .filter(item => !item.fork)
-      .reduce((pre, cur) => {
-        pre.total_issuses_count += cur.open_issues;
-        pre.total_star_count += cur.stargazers_count;
-        if (cur.stargazers_count !== 0 || cur.open_issues !== 0) {
-          pre.detail.push(`${cur.name} star: ${cur.stargazers_count} issuses: ${cur.open_issues}`);
-        }
-        return pre;
-      }, allData);
-    if (headers.Link.includes(`rel="next"`)) {
-      data = await fetchAllRepo(data, page + 1);
+const githubBookstrapOptions: GithubBootstrapOptions = {
+  key: 'github_username',
+  // 请修改此字段
+  username: ''
+};
+
+class GitHub {
+  baseUrl = 'https://api.github.com';
+  username: string;
+
+  constructor({ key, username }: GithubBootstrapOptions) {
+    this.username = cacheInit(key, username);
+  }
+
+  async bootstrap() {
+    try {
+      if (!this.username) {
+        $notify('GitHub', 'Warning', '请在脚本中设置 username 字段');
+        $done();
+      }
+      const [userInfo, repoInfo] = await Promise.all([this.fetchUserInfo(), this.fetchAllRepo()]);
+      $notify(
+        'GitHub',
+        `Star: ${repoInfo?.total_star_count} Issues: ${repoInfo?.total_issuses_count} Followers: ${userInfo?.followers} `,
+        repoInfo?.detail.join('\r\n')
+      );
+      $done();
+    } catch (err) {
+      $notify('GitHub', 'Error', err.message || err);
+      $done();
     }
-    return data;
-  } else {
-    return allData;
   }
-};
 
-const bootstrap = async () => {
-  try {
-    const [userInfo, repoInfo] = await Promise.all([fetchUserInfo(), fetchAllRepo()]);
-    $notify(
-      'GitHub',
-      `Star: ${repoInfo?.total_star_count} Issues: ${repoInfo?.total_issuses_count} Followers: ${userInfo?.followers} `,
-      repoInfo?.detail.join('\r\n')
-    );
-    $done();
-  } catch (err) {
-    $notify('GitHub Error', err.message || err);
-    $done();
+  private async fetchUserInfo() {
+    // https://docs.github.com/en/rest/reference/users#get-a-user
+    const getUserApi = `${this.baseUrl}/users/${this.username}`;
+    const { body } = await $task.fetch({ url: getUserApi });
+    const parseBody = JSON.parse(body) as UserInfo;
+    return parseBody;
   }
-};
 
-bootstrap();
+  private async fetchAllRepo(
+    allData = {
+      total_star_count: 0,
+      total_issuses_count: 0,
+      detail: [] as string[]
+    },
+    page = 1
+  ) {
+    // https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+    const listRepoApi = `${this.baseUrl}/users/${this.username}/repos?type=owner&per_page=100&page=${page}`;
+    const { body, headers } = await $task.fetch<ListRepoResponse>({ url: listRepoApi });
+    const parseBody = JSON.parse(body) as RepoInfo[];
+    if (body?.length > 0) {
+      let data = parseBody
+        .filter(item => !item.fork)
+        .reduce((pre, cur) => {
+          pre.total_issuses_count += cur.open_issues;
+          pre.total_star_count += cur.stargazers_count;
+          if (cur.stargazers_count !== 0 || cur.open_issues !== 0) {
+            pre.detail.push(`${cur.name} star: ${cur.stargazers_count} issuses: ${cur.open_issues}`);
+          }
+          return pre;
+        }, allData);
+      if (headers?.Link?.includes(`rel="next"`)) {
+        data = await this.fetchAllRepo(data, page + 1);
+      }
+      return data;
+    } else {
+      return allData;
+    }
+  }
+}
 
-export {};
+new GitHub(githubBookstrapOptions).bootstrap();
